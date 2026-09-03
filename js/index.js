7edd2ab64e386805185d3a79bda6ce5f191661b7
Simone <simonesnehvide@gmail.com>
2026-09-03 13:17:43 +0200

    tilføjede noget starter kode

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 // slå dig løs her... 
+const heroElement = document.querySelector(".hero")
+
+heros.forEach(function (hero) {
+    const imageElement = document.createElement("img");
+    imageElement
+
+    const headlineElement = document.createElement("h1");
+    headlineElement.textContent = hero.headline;
+
+    heroElement.append(imageElement, headlineElement);
+})
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline))
