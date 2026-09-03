6763b3b120ada9d013501d665b8a28545ff7f088
Simone <simonesnehvide@gmail.com>
2026-09-03 14:04:26 +0200

    Tilføjede lidt style og klasser

--- js/index.js
+++ js/index.js
@@ -3,15 +3,19 @@ const heroElement = document.querySelector(".hero")
 
 const imageElement = document.createElement("img");
 imageElement.src = hero.image;
+imageElement.classList.add("hero-image");
 
 const headlineElement = document.createElement("h1");
 headlineElement.textContent = hero.headline;
+headlineElement.classList.add("hero-headline");
 
 const copyElement = document.createElement("p");
 copyElement.textContent = hero.copy;
+copyElement.classList.add("hero-copy");
 
 const iconElement = document.createElement("img");
 iconElement.src = hero.icon;
+iconElement.classList.add("hero-icon");
 
 heroElement.append(
     imageElement,
